218dfb2b697b72eafe66681e4efefa3d9f73c2f4
GhaniC <abdelghanicherrak@oulook.fr>
2025-02-13 13:29:26 +0100

    VForward

--- Multi couche.py
+++ Multi couche.py
@@ -4,22 +4,13 @@ import time
 import random
 import matplotlib.pyplot as plt
 
-def sigmoid(x):
-    return 1 / 1 - np.exp(-x)
-
-def sigmoid_deriv(x):
-    return x * (1 - x)
-
-def produitmat(A,B):
-    return np.dot(A, B)
-
 
 
 t = time.time()
-train_data = pd.read_csv('C:/Users/Utilisateur/Documents/L3 TSE/Projet informatique magistère/mnist_train.csv')
-test_data = pd.read_csv("C:/Users/Utilisateur/Documents/L3 TSE/Projet informatique magistère/mnist_test.csv")
-#train_data = pd.read_csv('C:/Users/ghani/Documents/Desktop/TSE/Licence 3/S1/Projet Magistere/mnist_train.csv')
-#test_data = pd.read_csv('C:/Users/ghani/Documents/Desktop/TSE/Licence 3/S1/Projet Magistere/mnist_test.csv')
+#train_data = pd.read_csv('C:/Users/Utilisateur/Documents/L3 TSE/Projet informatique magistère/mnist_train.csv')
+#test_data = pd.read_csv("C:/Users/Utilisateur/Documents/L3 TSE/Projet informatique magistère/mnist_test.csv")
+train_data = pd.read_csv('C:/Users/ghani/Documents/Desktop/TSE/Licence 3/S1/Projet Magistere/mnist_train.csv')
+test_data = pd.read_csv('C:/Users/ghani/Documents/Desktop/TSE/Licence 3/S1/Projet Magistere/mnist_test.csv')
 
 #X_train=train_data.drop(labels=["label"],axis=1)
 #Y_train=train_data["label"]
@@ -38,49 +29,59 @@ X_test = X_test / 255.0
 print(time.time() - t)
 
 #Une classe Deux couches
-class DeuxCouches():
 
-    def __init__(self, input_size, output_size,a ,b learning_rate=0.1):
-        np.random.seed(42)
+import numpy as np
+
+
+class MLP:
+    def __init__(self, couches_sizes, learning_rate):
+        """
+        Initialise le MLP avec un nombre variable de couches et de neurones.
+
+        :param layer_sizes: Liste contenant le nombre de neurones par couche (entrée + cachées + sortie).
+        :param learning_rate: Taux d'apprentissage pour la mise à jour des poids.
+        """
+
+        self.couches_sizes = couches_sizes  # liste de dim par couche
         self.learning_rate = learning_rate
-        self.w = []
-        self.biais = []
-        self.a = a
+        self.L = len(couches_sizes) - 1  # Nombre de couches (sans la couche d'entrée)
 
-        #Initialisation en trois fois :
-        #couche d'entrée vers la première couche cachée
-        #couches cachées
-        #dernière couche cachée vers la sortie
-        self.w.append(np.random.randn(b, input_size))
-        self.biais.append(np.zeros((b, 1)))
+        self.weights = []
+        self.biases = []
 
-        for i in range(a - 1):
-            self.w.append(np.random.randn(b, b))
-            self.biais.append(np.zeros((b, 1)))
+        for l in range(self.L):
+            input_size = couches_sizes[l]
+            output_size = couches_sizes[l + 1]
+            self.weights.append(np.random.randn(input_size, output_size) * 0.01)
+            self.biases.append(np.random.randn(output_size) * 0.01)
 
-        self.w.append(np.random.randn(output_size, b))
-        self.biais.append(np.zeros((output_size, 1)))
+    def forward_propagation(self, M):
+        a = M
+        for l in range(self.L):
+            v = np.dot(a, self.weights[l]) + self.biases[l]
+            a = self.activation_function(v)
+        return a
 
+    def activation_function(self, v):
+        return 1 / (1 + np.exp(-v))
 
-    def backward(self, X, y):
-        m = X.shape[0]
-        erreurs = [None] * (self.a + 1)  # Liste pour stocker les erreurs
+    def softmax(self, v):
+        # Ensure v is a 2D array
+        if v.ndim == 1:
+            v = v.reshape(1, -1)
+        return np.exp(v) / np.sum(np.exp(v), axis=1, keepdims=True)
 
-        #Erreur en sortie
-        dA = (self.activations[-1] - y.T) * sigmoid_deriv(self.activations[-1])
-        erreurs[-1] = dA #donne l'erreur pour la derniere couche
-
-        #retropropagation des erreurs
-        for l in range(self.a, 0, -1):
-            erreurs[l - 1] = np.dot(self.w[l].T, erreurs[l]) * sigmoid_deriv(self.activations[l])
-
-        #mise à jour des poids et biais
-        for l in range(len(self.w)):
-            dW = np.dot(erreurs[l], self.activations[l].T) / m
-            db = np.sum(erreurs[l], axis=1, keepdims=True) / m
-            self.w[l] -= self.learning_rate * dW
-            self.biais[l] -= self.learning_rate * db
+    def prediction (self,I):
+        return self.softmax(self.forward_propagation(I))
 
 
 
+couches_sizes = [784,150,250,10]  # liste de dim par couche
+learning_rate = 0.1
 
+P = MLP (couches_sizes, learning_rate)
+
+test = [(X_test[i], Y_test[i]) for i in range(10000)]
+I = test[0][0]
+prediction = P.prediction(I)
+print(prediction)
