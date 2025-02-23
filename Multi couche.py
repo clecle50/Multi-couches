b5861b3d4322a4c146e15ea96351859c344fbca1
clecle50 <clemence.dsnx@gmail.com>
2025-02-23 11:51:20 +0100

    backward début

--- Multi couche.py
+++ Multi couche.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 
 
 t = time.time()
-#train_data = pd.read_csv('C:/Users/Utilisateur/Documents/L3 TSE/Projet informatique magistère/mnist_train.csv')
-#test_data = pd.read_csv("C:/Users/Utilisateur/Documents/L3 TSE/Projet informatique magistère/mnist_test.csv")
-train_data = pd.read_csv('C:/Users/ghani/Documents/Desktop/TSE/Licence 3/S1/Projet Magistere/mnist_train.csv')
-test_data = pd.read_csv('C:/Users/ghani/Documents/Desktop/TSE/Licence 3/S1/Projet Magistere/mnist_test.csv')
+train_data = pd.read_csv('C:/Users/Utilisateur/Documents/L3 TSE/Projet informatique magistère/mnist_train.csv')
+test_data = pd.read_csv("C:/Users/Utilisateur/Documents/L3 TSE/Projet informatique magistère/mnist_test.csv")
+#train_data = pd.read_csv('C:/Users/ghani/Documents/Desktop/TSE/Licence 3/S1/Projet Magistere/mnist_train.csv')
+#test_data = pd.read_csv('C:/Users/ghani/Documents/Desktop/TSE/Licence 3/S1/Projet Magistere/mnist_test.csv')
 
 #X_train=train_data.drop(labels=["label"],axis=1)
 #Y_train=train_data["label"]
@@ -35,12 +35,6 @@ import numpy as np
 
 class MLP:
     def __init__(self, couches_sizes, learning_rate):
-        """
-        Initialise le MLP avec un nombre variable de couches et de neurones.
-
-        :param layer_sizes: Liste contenant le nombre de neurones par couche (entrée + cachées + sortie).
-        :param learning_rate: Taux d'apprentissage pour la mise à jour des poids.
-        """
 
         self.couches_sizes = couches_sizes  # liste de dim par couche
         self.learning_rate = learning_rate
@@ -55,15 +49,11 @@ class MLP:
             self.weights.append(np.random.randn(input_size, output_size) * 0.01)
             self.biases.append(np.random.randn(output_size) * 0.01)
 
-    def forward_propagation(self, M):
-        a = M
-        for l in range(self.L):
-            v = np.dot(a, self.weights[l]) + self.biases[l]
-            a = self.activation_function(v)
-        return a
-
     def activation_function(self, v):
         return 1 / (1 + np.exp(-v))
+
+    def activation_derivative(self, a):
+        return a * (1 - a)
 
     def softmax(self, v):
         # Ensure v is a 2D array
@@ -71,9 +61,33 @@ class MLP:
             v = v.reshape(1, -1)
         return np.exp(v) / np.sum(np.exp(v), axis=1, keepdims=True)
 
-    def prediction (self,I):
-        return self.softmax(self.forward_propagation(I))
+    def forward_propagation(self, M):
+        activations = [M] #Stocke les activations des couches
+        zs = []  #Stocke les valeurs avant activation
 
+        a = M
+        for l in range(self.L-1): #j'ajoute -1 parce que la derniere se fait avec softmax dans prediction
+            v = np.dot(a, self.weights[l]) + self.biases[l]
+            zs.append(v)
+            a = self.activation_function(v)
+            activations.append(a)
+
+        return a,activations,zs
+
+    def prediction (self,I):
+        return self.softmax(self.forward_propagation(I)[0])
+
+    def back_propagation (self,X,Y):
+        m=X.shape[0]
+        activations, zs = self.forward_propagation(X)[1], self.forward_propagation(X)[2]
+
+        #Matrice des gradients, matrice mm forme mais avec zeros
+        d_weights = [np.zeros_like(w) for w in self.weights]
+        d_biases = [np.zeros_like(b) for b in self.biases]
+
+        #On transforme une valeur catégorielle en vecteur binaire
+        Y_one_hot = np.zeros((m, self.couches_sizes[-1]))
+        Y_one_hot[np.arange(m), Y] = 1
 
 
 couches_sizes = [784,150,250,10]  # liste de dim par couche
